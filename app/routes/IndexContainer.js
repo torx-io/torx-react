7fca3a81daf792310735fc0bfa3559dcd58058da
Tom Overton <thomasjohnoverton@gmail.com>
2016-07-04 07:20:23 -0700

    call UserPanel

--- app/routes/IndexContainer.js
+++ app/routes/IndexContainer.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import UserPanel from './components/UserPanel';
 
 class IndexContainer extends Component {
   render() {
     return (
       <div id='home-page' className='container'>
-        <h1>Test</h1>
-        <Link className='home-button' to='/about'><div className='home-button-container'><span className='home-button-text'>Link 1</span></div></Link>
-        <Link className='home-button' to='/about'><div className='home-button-container'><span className='home-button-text'>Link 2</span></div></Link>
-        <Link className='home-button' to='/about'><div className='home-button-container'><span className='home-button-text'>Link 3</span></div></Link>
+        <div>
+          <UserPanel />
+        </div>
       </div>
     );
   }
