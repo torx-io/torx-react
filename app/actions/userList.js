608c166af834b01467d0af4e860263c5226dac75
Tom Overton <thomasjohnoverton@gmail.com>
2016-07-05 03:45:20 -0700

    basic wiring for filter with mock data

--- app/actions/userList.js
+++ app/actions/userList.js
@@ -1,6 +1,6 @@
 import * as types from '../constants/ActionTypes';
-import fetch from 'isomorphic-fetch';
-import { clearEditingUser } from './user.js';
+// import fetch from 'isomorphic-fetch';
+// import { clearEditingUser } from './user.js';
 
 var usersMock = [
   {user_id: 0,first_name: 'First', last_name: 'User', email_address: 'user1@test.com'},
@@ -8,21 +8,6 @@ var usersMock = [
   {user_id: 2,first_name: 'Third', last_name: 'User', email_address: 'user3@test.com'},
   {user_id: 3,first_name: 'Fourth', last_name: 'User', email_address: 'user4@test.com'}
 ];
-
-export function postUser(user) {
-  return dispatch => {
-    return fetch('/api/v1/users/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(() => dispatch(clearEditingUser()))
-      .then(() => dispatch(getUsers()));
-  };
-}
 
 export function requestUsers() {
   return {
@@ -40,34 +25,48 @@ export function receiveUsers(users) {
 export function getUsers(filter) {
   return dispatch => {
     dispatch(requestUsers());
-    dispatch(receiveUsers(usersMock.filter(function (i,n){
-      return n.email === filter;
+    dispatch(receiveUsers(usersMock.filter(function (user){
+      return (user.email_address.match(filter) || user.first_name.match(filter) || user.last_name.match(filter));
     })));
   };
 }
 
-
-export function putUser(user) {
-  return dispatch => {
-    return fetch('/api/v1/users/' + user.user_id, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(() => dispatch(clearEditingUser()))
-      .then(() => dispatch(getUsers()));
-  };
-}
-
-export function deleteUser(user) {
-  return dispatch => {
-    return fetch('/api/v1/users/' + user.user_id, {
-      method: 'DELETE'
-    })
-      .then(() => dispatch(clearEditingUser()))
-      .then(() => dispatch(getUsers()));
-  };
-}
+// export function putUser(user) {
+//   return dispatch => {
+//     return fetch('/api/v1/users/' + user.user_id, {
+//       method: 'PUT',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(user)
+//     })
+//       .then(() => dispatch(clearEditingUser()))
+//       .then(() => dispatch(getUsers()));
+//   };
+// }
+//
+// export function postUser(user) {
+//   return dispatch => {
+//     return fetch('/api/v1/users/', {
+//       method: 'POST',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(user)
+//     })
+//       .then(() => dispatch(clearEditingUser()))
+//       .then(() => dispatch(getUsers()));
+//   };
+// }
+//
+// export function deleteUser(user) {
+//   return dispatch => {
+//     return fetch('/api/v1/users/' + user.user_id, {
+//       method: 'DELETE'
+//     })
+//       .then(() => dispatch(clearEditingUser()))
+//       .then(() => dispatch(getUsers()));
+//   };
+// }
